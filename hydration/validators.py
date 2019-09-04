a6c0a7dc2b17a60074a3ee9f049c4e20c8a4c1d9
Michael Shustin <michaelshustin@gmail.com>
2019-09-05 01:21:31 +0300

    [WIP] [#2] Implemented validators

---
+++ hydration/validators.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from typing import Any, Callable
+
+
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, value: Any) -> None:
+        """
+        Raises ValueError if the value is invalid.
+        :param value:                       The value to check
+        :return:                            None
+        :raises: :class:`ValueError`:       Value is invalid
+        """
+        pass
+
+
+class RangeValidator(Validator):
+    def __init__(self, range_obj: range):
+        """
+        :param range_obj:   A range object (returned by calling range())
+        """
+        self.range = range_obj
+
+    def validate(self, value: Any) -> None:
+        if value not in self.range:
+            raise ValueError('Given value {} is not in {}'.format(value, self.range))
+
+
+class ExactValueValidator(Validator):
+    def __init__(self, value: Any):
+        self.value = value
+        
+    def validate(self, value: Any) -> None:
+        if not self.value == value:
+            raise ValueError('Given value {} is not equal to {}'.format(value, self.value))
+
+
+class FunctionValidator(Validator):
+    def __init__(self, func: Callable):
+        """
+        :param func:    A callable that returns True if the value is valid
+        """
+        self.func = func
+
+    def validate(self, value: Any) -> None:
+        if not self.func(value):
+            raise ValueError('Calling {}({}) returned a False value'.format(self.func.__name__, value))
